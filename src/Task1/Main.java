19dfd69d81eb6fa6a2aa4b93d2584df49f2c3c7c
Alexander Lo <alexrrast@gmail.com>
2024-07-06 19:50:51 +0200

    - added version 3. logic of choosing the number of displayed examples

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,23 +1,42 @@
 package Task1;
 
 //Необходимо написать программу, которая сможет генерировать математические примеры для тренировки базовых навыков счета.
-//Версия 1. Программа выводит в консоль 20 случайно сгенерированых примеров на умножение двух двузначных чисел
+//Версия 1. Программа выводит в консоль 20 случайно сгенерированных примеров на умножение двух двузначных чисел
 
-//Версия 2. Программа проверяет, что среди примеров нет повторов. Если они есть, то вместо него генеририруется другой пример
+//Версия 2. Программа проверяет, что среди примеров нет повторов. Если они есть, то вместо него генерируется другой пример
 // и событие фиксируется выводом в консоль
 
+//Версия 3. Программа принимает параметры на вход
+//int - количество примеров
+//int - максимальная разрядность чисел
+//int - действия с числами (1- сложение, 2 - вычитание, 3 - умножение, 4 деление)
+
+
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите желаемое количество примеров");
+        int problemsNumber = 0;
+        try {
+            problemsNumber = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка. Введено не число");
+        }
+
 
         Random random = new Random();
         ArrayCreator numbers = new ArrayCreator();
 
         int[] arr = numbers.createArray();
-        String[] storage = new String[20];
+        String[] storage = new String[problemsNumber];
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < problemsNumber; i++) {
             int randomValue1 = arr[random.nextInt(arr.length)];
             int randomValue2 = arr[random.nextInt(arr.length)];
             storage[i] = (randomValue1 + " * " + randomValue2 + " =");
