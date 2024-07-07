882b7ccc45ea2995066c450c6319a3323d86760b
Alexander Lo <alexrrast@gmail.com>
2024-07-07 15:34:44 +0200

    - added logic for selecting math operation

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -22,52 +22,63 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите желаемое количество примеров");
-        int problemsNumber = 0;
+        int problemsNumber;
         try {
             problemsNumber = scanner.nextInt();
         } catch (InputMismatchException e) {
             System.out.println("Ошибка. Введено не число");
+            return;
         }
         String[] storage = new String[problemsNumber];
 
         Random random = new Random();
         ArrayCreator numbers = new ArrayCreator();
-        int[] arr = new int[0];
+        int[] arr;
 
         System.out.println("Введите разрядность чисел примеров (от 2 до 5)");
         int digitNumber;
         try {
             digitNumber = scanner.nextInt();
-            if (digitNumber == 2) {
-                arr = numbers.createTwoDigitArray();
-            } else if (digitNumber == 3) {
-                arr = numbers.createThreeDigitArray();
-            } else if (digitNumber == 4) {
-                arr = numbers.createFourDigitArray();
-            } else if (digitNumber == 5) {
-                arr = numbers.createFiveDigitArray();
-            } else {
-                System.out.println("Будьте внимательнее, поддерживается разрядность чисел от 2 до 5");
-                return;
+            switch (digitNumber) {
+                case (2) -> arr = numbers.createTwoDigitArray();
+                case (3) -> arr = numbers.createThreeDigitArray();
+                case (4) -> arr = numbers.createFourDigitArray();
+                case (5) -> arr = numbers.createFiveDigitArray();
+                default -> {
+                    System.out.println("Будьте внимательнее, поддерживается разрядность чисел от 2 до 5");
+                    return;
+                }
             }
-  /*          switch (digitNumber) {
-                case (2):
-                    arr = numbers.createTwoDigitArray();
-                case (3):
-                    arr = numbers.createThreeDigitArray();
-                case (4):
-                    arr = numbers.createFourDigitArray();
-                case (5):
-                    arr = numbers.createFiveDigitArray(); */
-// switch-case почему то всегда вызывал .createFiveDigitArray(), не смог понять почему
         } catch (InputMismatchException e) {
             System.out.println("Ошибка. Введено не число");
+            return;
+        }
+
+        System.out.println("Какое математическое действие прорешиваем? 1 - сложение, 2 - вычитание, 3 - умножение, 4 деление");
+        String mathOperation;
+
+        try {
+            int usersMathOperationInput = scanner.nextInt();
+
+            switch (usersMathOperationInput) {
+                case (1) -> mathOperation = "+";
+                case (2) -> mathOperation = "-";
+                case (3) -> mathOperation = "*";
+                case (4) -> mathOperation = ":";
+                default -> {
+                    System.out.println("Будьте внимательнее, нужно ввести значение от 1 до 4");
+                    return;
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка. Введено не число");
+            return;
         }
 
         for (int i = 0; i < problemsNumber; i++) {
             int randomValue1 = arr[random.nextInt(arr.length)];
             int randomValue2 = arr[random.nextInt(arr.length)];
-            storage[i] = (randomValue1 + " * " + randomValue2 + " =");
+            storage[i] = (randomValue1 + " " + mathOperation + " " + randomValue2 + " =");
         }
 
         // trying to find duplicates and replace it
@@ -75,7 +86,7 @@ public class Main {
             for (int j = i + 1; j < storage.length; j++) {
                 if (storage[i].equals(storage[j])) {
                     System.out.println("Дубликат найден: " + storage[i] + " на позициях в массиве " + i + " и " + j);
-                    storage[j] = arr[random.nextInt(arr.length)] + " * " + arr[random.nextInt(arr.length)] + " =";
+                    storage[j] = arr[random.nextInt(arr.length)] + " " + mathOperation + " " + arr[random.nextInt(arr.length)] + " =";
                 }
             }
             System.out.println((i + 1) + ". " + storage[i]);
